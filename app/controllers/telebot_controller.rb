a149dee1d9b0b37fbcdd6425a490b54fc990baff
angkiki <gabrielangzw@gmail.com>
2018-10-04 13:41:57 +0800

    initial configurations for receiving webhook

---
+++ app/controllers/telebot_controller.rb
@@ -0,0 +1,25 @@
+class TelebotController < ApplicationController
+  skip_before_action :verify_authenticity_token
+
+  def telebot_webhook
+    if params['message']
+      @chat_id = params['message']['chat']['id']
+      @command = params['message']['text']
+
+      @response = {
+        "method": "sendMessage",
+        "chat_id": @chat_id,
+        "text": "Hello! :)"
+      }
+
+      respond_to do |format|
+        format.json { render json: @response, status: 200 }
+      end
+    else
+      respond_to do |format|
+        format.json { render json: {error: 'Error'}, status: 422 }
+      end
+    end
+  end
+
+end
