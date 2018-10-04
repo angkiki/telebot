fceaf9967ede02618138546fec84eba713baee2d
angkiki <gabrielangzw@gmail.com>
2018-10-04 18:58:17 +0800

    added log to check incoming username

--- app/controllers/telebot_controller.rb
+++ app/controllers/telebot_controller.rb
@@ -22,6 +22,7 @@ class TelebotController < ApplicationController
         render json: @response, status: 200
       else
         # first time user is talking to our bot
+        puts "USERNAME: #{@username}"
         Chat.new_chat(@chat_id, @username)
 
         # respond with first time greeting
