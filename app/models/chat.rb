deeb6924a23954f4790bb2ea9352177e074d27cd
angkiki <gabrielangzw@gmail.com>
2018-10-09 13:28:10 +0800

    added chat model method to tabulate spendings

--- app/models/chat.rb
+++ app/models/chat.rb
@@ -9,4 +9,24 @@ class Chat < ActiveRecord::Base
     chat.update_attributes(command: new_command)
   end
 
+  def total_spendings(month)
+    @transactions = self.transactions.where('extract(month from created_at) = ?', month)
+    @spendings = { food: 0.0, shopping: 0.0, transport: 0.0, misc: 0.0 }
+
+    @transactions.each do |t|
+      case t.category
+      when '/food'
+        @spendings[:food] += 1
+      when '/shopping'
+        @spendings[:shopping] += 1
+      when '/transport'
+        @spendings[:transport] += 1
+      when '/misc'
+        @spendings[:misc] += 1
+      end
+    end
+
+    return @spendings
+  end
+
 end
