cfb6a1109c08c9c38b9f31298864ef082a710535
angkiki <gabrielangzw@gmail.com>
2018-10-31 10:42:28 +0800

    changed rails timezone to singapore

--- config/application.rb
+++ config/application.rb
@@ -15,5 +15,7 @@ module RailsTelebot
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
+
+    config.time_zone = "Singapore"
   end
 end
