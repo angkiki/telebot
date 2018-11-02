2cf72ce162e083e264ee276b56d58c335b85de10
angkiki <gabrielangzw@gmail.com>
2018-11-02 10:46:03 +0800

    added basic form and routes for displaying user information

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,4 +3,9 @@ class HomeController < ApplicationController
   def home
   end
 
+  def show_user
+    @chat = Chat.find_by(chat_id: params[:chat_id])
+    redirect_to root_path if !@chat
+  end
+
 end
