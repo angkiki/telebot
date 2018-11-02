7d17dcf15e9b881bd6c144b0627d6626dddc5444
angkiki <gabrielangzw@gmail.com>
2018-11-02 13:39:42 +0800

    added views to display spendings

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -6,6 +6,10 @@ class HomeController < ApplicationController
   def show_user
     @chat = Chat.find_by(chat_id: params[:chat_id])
     redirect_to root_path if !@chat
+
+    @start_date = DateTime.new(params[:date][:year].to_i, params[:date][:month].to_i)
+    @end_date = @start_date.end_of_month
+    @transactions = @chat.transactions.where(created_at: @start_date..@end_date)
   end
 
 end
