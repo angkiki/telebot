cdef2a7f9e9981a180b2cee063b451523a2955df
angkiki <gabrielangzw@gmail.com>
2018-11-09 13:47:56 +0800

    updated styling for user gui and added model method to display custom css classes

--- app/models/transaction.rb
+++ app/models/transaction.rb
@@ -6,4 +6,13 @@ class Transaction < ApplicationRecord
   def validates_amount
     errors.add(:amount, "Amount cannot be less than 0") if self.amount <= 0
   end
+
+  # method for displaying custom classes for use in home#show_user.html.erb
+  def display_class
+    return "user-tr__red" if self.amount >= 50
+    return "user-tr__orange" if self.amount >= 25
+    return "user-tr__green" if self.amount >= 10
+
+    nil
+  end
 end
